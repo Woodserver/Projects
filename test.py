575b48b4a97495165b4ad9f03fc30b3290e9de2e
Woodserver <54159876+Woodserver@users.noreply.github.com>
2021-09-20 15:05:05 -0400

    able to sort through each line to see which are numbers

---
+++ test.py
@@ -0,0 +1,14 @@
+test = "suck it"
+print (test[2])
+    
+    
+    #this will be how we write to the final txt file. below is the fomat
+    #vcardfile.write is the method used
+    #vcardfile.write(spreadsheet.cell_value(datalines, 0))
+
+    #(f"""BEGIN:VCARD
+    #VERSION:3.0
+    #FN:{lastname}, {firstname}
+    #TEL;TYPE=VOICE,CELL;VALUE=text:{phonenumber}
+    #END:VCARD
+    #""")
--- vcard.py
+++ vcard.py
@@ -2,30 +2,26 @@ import xlrd
 
 #specifing where the data is located on the machine
 location = ('Book1.xls')
-vcardfile = open('vcard-non-apple.txt','w+')
+vcardfile = open('vcard-non-apple.txt','a+')
 workbook = xlrd.open_workbook(location)
 spreadsheet = workbook.sheet_by_index(0)
+fullname = ""
 firstname = ""
 lastname = ""
 phonenumber = ""
 
 
- #printing all first rows on each column
+ ###Sorting each dataline in the spreadsheet. Checking if the 3rd value is a number
 for datalines in range(spreadsheet.nrows):
-    if spreadsheet.[2] == :
-        print ('omge its 9')
+    if (spreadsheet.cell_value(datalines, 0))[2] == '9'or'8'or'7'or'6'or'5'or'4'or'3'or'2'or'1'or'0':
+        phonenumber = (spreadsheet.cell_value(datalines, 0))
+    else:
+        fullname = (spreadsheet.cell_value(datalines, 0))
 
-    
-    
-    
-    
-    #this will be how we write to the final txt file. below is the fomat
-    #vcardfile.write is the method used
-    #vcardfile.write(spreadsheet.cell_value(datalines, 0))
 
-    #(f"""BEGIN:VCARD
-    #VERSION:3.0
-    #FN:{lastname}, {firstname}
-    #TEL;TYPE=VOICE,CELL;VALUE=text:{phonenumber}
-    #END:VCARD
-    #""")
+    print(f"""BEGIN:VCARD
+        VERSION:3.0
+        FN:{lastname}, {fullname}
+        TEL;TYPE=VOICE,CELL;VALUE=text:{phonenumber}
+        END:VCARD
+        """)
