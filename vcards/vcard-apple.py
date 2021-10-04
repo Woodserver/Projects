2358571449b3659a51415d7e77825035d03cad16
Woodserver <54159876+Woodserver@users.noreply.github.com>
2021-10-04 15:23:31 -0400

    finalized project and provided a test spreadsheet for proof of concept

--- vcards/vcard-apple.py
+++ vcards/vcard-apple.py
@@ -1,5 +1,5 @@
 import xlrd
-location = ('vcards/Book1.xlsx')                 #point the script to the location of the spreadsheet
+location = ('vcards\Book1.xlsx')                 #point the script to the location of the spreadsheet
 vcardfile = open('vcard-apple.vcf','a+')
 workbook = xlrd.open_workbook(location)
 spreadsheet = workbook.sheet_by_index(0)
@@ -18,12 +18,12 @@ END:VCARD""")
 
 #The next two for loops processes the data in the spreadsheet by appending every other value to either the
 #userphonenumber_list or userfullname_list
-for datalines in range(spreadsheet.nrows)[1::2]:
+for datalines in range(spreadsheet.nrows):
     phonenumbers_filtered_from_spreadsheet = spreadsheet.cell_value(datalines, 0)
     userphonenumber_list.append(phonenumbers_filtered_from_spreadsheet)
 #grabbing the numbers mason
-for datalines in range(spreadsheet.nrows)[::2]:
-    fullnames_filtered_from_spreadsheet = spreadsheet.cell_value(datalines, 0)
+for datalines in range(spreadsheet.nrows):
+    fullnames_filtered_from_spreadsheet = spreadsheet.cell_value(datalines, 1)
     userfullname_list.append(fullnames_filtered_from_spreadsheet)
 
 #now we zip both lists into a single for loop to finally create the vcard
