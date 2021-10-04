2dbbbfc9e227ecde693bd788ec5e40f38db9282c
Woodserver <54159876+Woodserver@users.noreply.github.com>
2021-10-04 14:27:51 -0400

    cleaned up a bit

---
+++ vcards/vcard-non-apple.py
@@ -0,0 +1,35 @@
+import xlrd
+location = ('vcards/Book1.xlsx')                 #point the script to the location of the spreadsheet
+vcardfile = open('vcard-non-apple.vcf','a+')
+workbook = xlrd.open_workbook(location)
+spreadsheet = workbook.sheet_by_index(0)
+fullname = ''
+userphonenumber_list = []
+userfullname_list = []
+
+def write_to_vcard(lastname, firstname, phonenumber):
+    vcardfile.write(f"""
+BEGIN:VCARD
+VERSION:3.0
+FN:{lastname}, {firstname}
+TEL;TYPE=VOICE,CELL;VALUE=text:{phonenumber}
+END:VCARD""")
+
+#The next two for loops processes the data in the spreadsheet by appending every other value to either the
+#userphonenumber_list or userfullname_list
+for datalines in range(spreadsheet.nrows)[1::2]:
+    phonenumbers_filtered_from_spreadsheet = spreadsheet.cell_value(datalines, 0)
+    userphonenumber_list.append(phonenumbers_filtered_from_spreadsheet)
+#grabbing the numbers mason
+for datalines in range(spreadsheet.nrows)[::2]:
+    fullnames_filtered_from_spreadsheet = spreadsheet.cell_value(datalines, 0)
+    userfullname_list.append(fullnames_filtered_from_spreadsheet)
+
+#now we zip both lists into a single for loop to finally create the vcard
+zip_both_lists = zip(userfullname_list, userphonenumber_list)
+for user_fullnames, user_phonenumbers in zip_both_lists:
+    phonenumber = user_phonenumbers
+    fullname = user_fullnames
+    firstname = fullname.split()[0]
+    lastname = fullname.split()[-1]
+    write_to_vcard(lastname, firstname, phonenumber)
